db779affdd6548b29b73255fb59d51af7f2f31a7
Emerson <eemr3@yahoo.com.br>
2022-01-28 16:31:07 -0300

    feat(Footer/Redirect): Redirecione a pessoa usuária para a tela de explorar ao clicar no ícone de exploração requisito-23

--- src/pages/Explore/index.js
+++ src/pages/Explore/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import Footer from '../../componentes/Footer';
 
 function Explore() {
   return (
-    <div><h1>Explore</h1></div>
+    <div>
+      <Footer />
+    </div>
   );
 }
 
