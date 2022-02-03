70962f17cd686bd6ea65f8bc7095bb2304f5719f
Emerson <eemr3@yahoo.com.br>
2022-02-03 00:43:15 -0300

    feat(InProgressDrinks): implementa tela de progresso Drinks -  requisito 47,48e49

--- src/pages/InProgressDrink/index.js
+++ src/pages/InProgressDrink/index.js
@@ -1,11 +1,81 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Button, Container } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+import CardDetailsandProgress from '../../components/CardDetailsandProgress';
+import ListIgredients from '../../components/ListIgredients';
+import { requestCocktailById } from '../../services/requestsApi';
 
-// import { Container } from './styles';
+function InProgressDrink({ match }) {
+  const [getRecipeForRende, setGetRecipeForRende] = useState({});
+  const [igredientsMeasures, setIgredientsMeasures] = useState([]);
 
-function InProgressDrink() {
+  useEffect(() => {
+    const { id } = match.params;
+    const getCocktailById = async () => {
+      const response = await requestCocktailById(id);
+      setGetRecipeForRende(response[0]);
+    };
+    getCocktailById();
+  }, [match.params]);
+
+  useEffect(() => {
+    const ingredientAndMeasure = () => {
+      const igrendientAndMeasure = [];
+      const igredient = Object.keys(getRecipeForRende)
+        .map((key) => (key.includes('strIngredient')
+        && getRecipeForRende[key] !== '' && getRecipeForRende[key]))
+        .filter((item) => item !== false && item !== null);
+
+      const measure = Object.keys(getRecipeForRende)
+        .map((key) => (key.includes('strMeasure')
+      && getRecipeForRende[key] !== ' ' && getRecipeForRende[key]))
+        .filter((item) => item !== false && item !== null);
+      for (let index = 0; index < igredient.length; index += 1) {
+        igrendientAndMeasure.push([igredient[index], measure[index]]);
+      }
+      setIgredientsMeasures(igrendientAndMeasure);
+    };
+    ingredientAndMeasure();
+  }, [getRecipeForRende]);
+
   return (
-    <div><h1>In Progress Drink</h1></div>
+    igredientsMeasures.length > 0 ? (
+      <Container>
+        <CardDetailsandProgress
+          image={ getRecipeForRende.strDrinkThumb }
+          title={ getRecipeForRende.strDrink }
+          category={ getRecipeForRende.strCategory }
+          instructions={ getRecipeForRende.strInstructions }
+          dataTestIdTitle="recipe-title"
+          dataTestImg="recipe-photo"
+          dataTestIdCategory="recipe-category"
+          dataTestIdInstruction="instructions"
+        />
+        <ul>
+          {igredientsMeasures.map((igred, index) => (
+            <ListIgredients
+              key={ igred[0] }
+              igredient={ igred[0] }
+              measure={ igred[1] }
+              index={ index }
+              isvisibility
+            />
+          ))}
+
+        </ul>
+        <Button type="button" data-testid="finish-recipe-btn">Finalizar receita</Button>
+      </Container>
+
+    ) : (<p>Loading...</p>)
+
   );
 }
 
+InProgressDrink.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
 export default InProgressDrink;
