2079488581d228e9f47c0c4d26b389c96b0755e5
Gabriel Borges <igblborges@gmail.com>
2022-02-07 15:50:30 -0300

    fix:(datails foods)delete de button provisorio

--- src/pages/DetailsFood/index.js
+++ src/pages/DetailsFood/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
 
 // import { Container } from './styles';
 
 function DetailsFood() {
-  const history = useHistory();
   return (
     <div>
       <h1>Details Food</h1>
-      <button type="button" onClick={ () => history.push('/done-recipes') }>
-        DoneRecipe
-      </button>
+
     </div>
   );
 }
